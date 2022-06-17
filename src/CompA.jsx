e474d88479812b1f3984087200ac0d18598ea8f5
Mayank Verma <vermamayank564@gmail.com>
2022-06-17 16:37:54 +0530

    -useEffect ( dependency change using ref)

--- src/CompA.jsx
+++ src/CompA.jsx
@@ -1,20 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 const Input = () => {
   const [state, setState] = useState(0);
+  const ref = useRef(0);
   useEffect(() => {
-    console.log("mouting", state);
-
-    return () => {
-      console.log("unmouting", state);
-    };
-  }, [state]);
+    console.log("ref", ref.current);
+  }, [ref.current]);
 
   return (
     <div>
       Input
       <button
         onClick={() => {
-          setState(state + 1);
+          // setState(state + 1);
+          ref.current = ref.current + 1;
         }}
       >
         first
